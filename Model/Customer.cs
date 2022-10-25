488db2fb53a32887bba17e7c5282f401e5a8d4c0
htariq <hasan_tariq84@hotmail.com>
2022-10-25 13:38:04 +0500

    commint whole project

---
+++ Model/Customer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Model
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string _firstName;
+        private string _lastName;
+        private string _amount;
+        private bool _married;
+
+        public int Amount{ get; set; }
+        public string CustomerFirstName { get { return _firstName; }
+        set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    RaisePropertyChanged("CustomerFirstName");
+                    RaisePropertyChanged("CustomerFullName");
+
+                }
+            }
+        }
+
+        public string CustomerLastName {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    RaisePropertyChanged("CustomerFullName");
+                    RaisePropertyChanged("CustomerLastName");
+                }
+            }
+        }
+
+        public string CustomerFullName { get { return CustomerFirstName + " " + CustomerLastName; }  }
+        public bool Married
+        {
+            get { return _married; }
+            set
+            {
+                if (_married != value)
+                {
+                    _married = value;
+                    RaisePropertyChanged("Married");
+                }
+            }
+        }
+
+        private double _Tax;
+
+        
+
+        public double Tax
+        {
+            get { return _Tax; }
+        }
+
+        public void CalculateTax()
+        {
+            if (Amount > 2000)
+            {
+                _Tax = 20;
+                
+            }
+            else if (Amount > 1000)
+            {
+                _Tax = 10;
+            }
+            else
+            {
+                _Tax = 5;
+            }
+
+            RaisePropertyChanged("Tax");
+        }
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
+
---
+++ ViewModel/CustomerVM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM_Model;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+namespace MVVM_ViewModel
+{
+    public class CustomerVM
+    {
+
+        //public ObservableCollection<Customer> obj = new ObservableCollection<Customer>();
+
+        public CustomerVM()
+        {
+            LoadCustomers();
+        }
+
+        public ObservableCollection<Customer> CustomerList
+        {
+            get; set;
+        }
+
+        public void LoadCustomers ()
+        {
+            ObservableCollection<Customer> custObj = new ObservableCollection<Customer>();
+
+            custObj.Add(new Customer { CustomerFirstName = "Hassan", CustomerLastName = "Tariq", Amount = 30000, Married = true });
+            custObj.Add(new Customer { CustomerFirstName = "Ali", CustomerLastName = "Tufail", Amount = 200, Married = false });
+            custObj.Add(new Customer { CustomerFirstName = "Akram", CustomerLastName = "Rahi", Amount = 50000, Married = true });
+            custObj.Add(new Customer { CustomerFirstName = "Suriya", CustomerLastName = "Bajiya", Amount = 400, Married = false});
+            custObj.Add(new Customer { CustomerFirstName = "Sana", CustomerLastName = "Tahir", Amount = 5000, Married = false });
+
+            CustomerList = custObj;
+        }
+
+        
+    }
+
+
+}
